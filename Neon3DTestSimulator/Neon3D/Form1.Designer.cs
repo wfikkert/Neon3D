6fc518dd691d53ff7c3220c69aedfc9eaa046614
EldinZenderink <s1077709@student.windesheim.nl>
2016-02-22 13:44:53 +0100

    c# project to test algoritms
    Testing formules and algoritms, will later become a modeller for
    creating 3D models to upload to the FPGA.
    
    Initial program:
    - drawPixel
    - removePixel
    - drawLine

---
+++ Neon3DTestSimulator/Neon3D/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Neon3D
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Show3D = new System.Windows.Forms.Button();
+            this.Debug = new System.Windows.Forms.RichTextBox();
+            this.Coordinates = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Show3D
+            // 
+            this.Show3D.Location = new System.Drawing.Point(966, 354);
+            this.Show3D.Name = "Show3D";
+            this.Show3D.Size = new System.Drawing.Size(312, 23);
+            this.Show3D.TabIndex = 0;
+            this.Show3D.Text = "Show3D";
+            this.Show3D.UseVisualStyleBackColor = true;
+            this.Show3D.Click += new System.EventHandler(this.Show3D_Click);
+            // 
+            // Debug
+            // 
+            this.Debug.Location = new System.Drawing.Point(966, 0);
+            this.Debug.Name = "Debug";
+            this.Debug.Size = new System.Drawing.Size(312, 275);
+            this.Debug.TabIndex = 1;
+            this.Debug.Text = "";
+            // 
+            // Coordinates
+            // 
+            this.Coordinates.Location = new System.Drawing.Point(966, 328);
+            this.Coordinates.Name = "Coordinates";
+            this.Coordinates.Size = new System.Drawing.Size(312, 20);
+            this.Coordinates.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(963, 312);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Coordinates";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1279, 588);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Coordinates);
+            this.Controls.Add(this.Debug);
+            this.Controls.Add(this.Show3D);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Show3D;
+        private System.Windows.Forms.RichTextBox Debug;
+        private System.Windows.Forms.TextBox Coordinates;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Neon3DTestSimulator/Neon3D/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Neon3D
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] previousCor = { "0", "0", "0", "0" };
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Show3D_Click(object sender, EventArgs e)
+        {
+
+            drawLine(Int32.Parse(previousCor[0]), Int32.Parse(previousCor[1]), Int32.Parse(previousCor[2]), Convert.ToDouble(previousCor[3]), true);
+            string cor;
+            string[] corArray = { "0", "0", "0", "0"};
+            if ((cor = Coordinates.Text) != "")
+            {
+                Debug.AppendText("Coordinates set:  " + cor + "\n");
+                corArray = cor.Split(',');
+                previousCor = corArray;
+            }
+            
+            drawLine((double)Int32.Parse(corArray[0]), (double)Int32.Parse(corArray[1]), (double)Int32.Parse(corArray[2]), Convert.ToDouble(corArray[3]), false);
+        }
+
+        private void drawLine(double x1, double y1, double x2, double y2, bool remove)
+        {
+            Debug.AppendText("Start drawline \n");
+            double x;
+            double prevY = 0;
+            double prevX = 0;
+            for(x = x1; x <= x2; x++)
+            {
+
+                //double y = ((y2 / x2) - (y1 / y2)) * x + y1;
+                double y;
+                try
+                {
+                    Debug.AppendText("running calculation \n");
+                    y = ((y2 - y1) / x2) * (x - x1) + y1;
+                }
+                catch
+                {
+                    y = 0;
+                }
+                
+                y = Math.Round(y);
+                if(y - prevY > 0 && x != 0)
+                {
+                     
+                    int counter;
+                    for(counter = (int)prevY; counter <= (int)y; counter++)
+                    {
+                        if (!remove)
+                        {
+                            drawPixel((int)prevX, counter);
+                        }
+                        else
+                        {
+                            removePixel((int)prevX, counter);
+                        }
+                    }
+                }
+
+                if (!remove)
+                {
+                    Debug.AppendText("DRAW PIXEL AT: " + (int)x + ", " + (int) y + " \n");
+                    drawPixel((int)x, (int)y);
+                } else
+                {
+                    removePixel((int)x, (int)y);
+                }
+
+                prevY = y;
+                prevX = x;
+            }
+            
+
+        }
+        
+
+        private void drawPixel(int x, int y)
+        {
+            Brush aBrush = (Brush)Brushes.Black;
+            Graphics g = this.CreateGraphics();
+
+            g.FillRectangle(aBrush, x, y, 1, 1);
+        }
+
+        private void removePixel(int x, int y)
+        {
+
+            Brush aBrush = (Brush)Brushes.White;
+            Graphics g = this.CreateGraphics();
+            if(x != 0 && y != 0)
+            {
+                g.FillRectangle(aBrush, x, y, 1, 1);
+            }
+           
+        }
+    }
+}
